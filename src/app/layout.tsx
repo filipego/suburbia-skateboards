1326607f0e8f337bcfd3f53a61d27072db9c3640
Filipe Valente Gomes <filipego@gmail.com>
2025-01-09 18:08:11 -0500

    added footer

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,6 +2,7 @@ import type { Metadata } from "next";
 import { Bowlby_One_SC, DM_Mono } from "next/font/google";
 import "./globals.css";
 import { Header } from "@/components/Header";
+import { Footer } from "@/components/Footer";
 import { SVGFilters } from "@/components/SVGFilters";
 
 const bowlby = Bowlby_One_SC({
@@ -37,6 +38,7 @@ export default function RootLayout({
         <main>
           <Header />
           {children}
+          <Footer />
         </main>
         <SVGFilters />
       </body>
